79d62b806c6eff965fee6fe0583321b20713cf50
Z3LT1K09 <erickromanvazquez@gmail.com>
2025-07-18 12:08:36 -0600

    [DEV] v1.0.0.4 published!

--- Wialon.Tests/Tester.cs
+++ Wialon.Tests/Tester.cs
@@ -32,16 +32,26 @@ namespace Wialon.Tests
             SearchItemsResult<UnitGroup> resultGroups = await wialonClient.SearchItems<UnitGroup>(allgroups);
             List<UnitGroup> unitGroupSectors = resultGroups.items.Where(x => x.nm.Contains("Sector")).ToList();
 
+            UnitGroup sector4 = unitGroupSectors.First(x => x.nm == "Sector 4");
+            UnitGroup sector2 = unitGroupSectors.First(x => x.nm == "Sector 2");
 
-            SearchItemsParams searchAllZones = SearchParamsFactory.AllZones();
-            var result = await wialonClient.SearchItems<GeoFenceGroup>(searchAllZones);
-            GeoFenceGroup geoFenceGroup = result.items.First();
+            long[] insertTest = [];
+            object x = new { itemId = sector4.id, units = insertTest };
 
-            Dictionary<string, GeoFence> geoFences = geoFenceGroup.zl.Where(x => x.Value.n.Contains("SECTOR")).ToDictionary();
-            geoFenceGroup.zl = geoFences;
+            string result = await wialonClient.RawRequest("unit_group/update_units", x);
 
-            Dictionary<string, long[]> pointInGeoF = await wialonClient.GeofencesByPoint(20.986225, -101.479373, geoFenceGroup);
-            var x = 3;
+
+            
+            
+            
+            //SearchItemsParams searchAllZones = SearchParamsFactory.AllZones();
+            //var result = await wialonClient.SearchItems<GeoFenceGroup>(searchAllZones);
+            //GeoFenceGroup geoFenceGroup = result.items.First();
+
+            //Dictionary<string, GeoFence> geoFences = geoFenceGroup.zl.Where(x => x.Value.n.Contains("SECTOR")).ToDictionary();
+            //geoFenceGroup.zl = geoFences;
+
+            //Dictionary<string, long[]> pointInGeoF = await wialonClient.GeofencesByPoint(20.986225, -101.479373, geoFenceGroup);
         }
     }
 }
